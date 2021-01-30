e8c1ab8b5dcbe65ea1515f61ccc6e02e333eaf42
Riccardo Nannini <riccardo1.nannini@mail.polimi.it>
2021-01-30 13:46:09 +0100

    Admin Checker filter + admin page creation
    Admin Checker filter that works on admin related servlets + admin home page creation

---
+++ GamifiedMarketingApplicationWEB/src/it/polimi/db2/controllers/GoToLeaderboardPage.java
@@ -0,0 +1,45 @@
+package it.polimi.db2.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+
+@WebServlet("/GoToLeaderboardPage")
+public class GoToLeaderboardPage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	private TemplateEngine templateEngine;
+	
+    public GoToLeaderboardPage() {
+        super();
+    }
+
+	public void init() throws ServletException {
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+	}
+
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
